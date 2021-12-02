a570d7a91a50ac8a49a846217175f9e06119800e
Charlie Fordon <fordoncf@gmail.com>
2021-12-02 10:55:02 -0500

    completed three

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function(str) {
 
+const reverseString = function(str) {
+let stringArray = []
+let newString = ""
+    for (i=0; i<=(str.length - 1); i++) {
+        stringArray.push(str.charAt(i))
+    }
+    for (j=(str.length - 1); j>=0; j--) {
+        newString = newString + stringArray[j]
+    }
+return(newString)
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
