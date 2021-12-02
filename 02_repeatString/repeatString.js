3464cf0ede85107bf4b534405ca62705f6dcdc11
Charlie Fordon <fordoncf@gmail.com>
2021-12-02 09:55:37 -0500

    task one and two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
 
+const repeatString = function(x, y) {
+   let string = ""
+   if (y<0) {
+       return("ERROR")
+   }
+   else {
+    for(i=0; i<y; i++) {
+        string = string+x
+    }
+    return(string)
+}
 };
 
 // Do not edit below this line
